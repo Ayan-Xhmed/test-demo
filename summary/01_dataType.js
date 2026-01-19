893e5f4ea6d13a43b5e9fed6a196800835cefba4
Ayan-Xhmed <ayanahmed9760@gmail.com>
2026-01-19 16:27:30 +0000

    summary

---
+++ summary/01_dataType.js
@@ -0,0 +1,21 @@
+// Primitive
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+const score = "100"
+const scoreValue = 100.3
+const isLoggedIn = false
+const outSideTemp = null
+const userEmail = undefined;
+const id = Symbol('1116');
+const bigNumber = 3553515434318n
+
+// Reference (Non primitive)
+// Array, Object, Functions
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj ={
+    name: "ayan",
+    age:21
+}
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
