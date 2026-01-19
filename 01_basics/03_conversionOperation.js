f9e759f11c80648a6ba8bf4084355afa52f2c617
Ayan-Xhmed <ayanahmed9760@gmail.com>
2026-01-19 02:09:29 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,29 +1,25 @@
-// let score = "33"
+ let score = true //undefined //null //"33abc" //"33" //33
 
 // //const {score} = req.body //we know on line 1 type is Number but on line 3 not sure
-//  console.log(typeof score);
-//  console.log(typeof (score));
- 
-//  let valueInt
 
-let score = "hitesh"
-
-//console.log(typeof score);
-//console.log(typeof(score));
+ console.log(score);
+ console.log(typeof (score));
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 
 // "33" => 33
-// "33abc" => NaN
+// "33abc" => NaN =>type is = number
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn =0// "ayan" //""//1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//  console.log(booleanIsLoggedIn);
+ console.log(booleanIsLoggedIn);
+ console.log(typeof booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -32,14 +28,14 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -49,26 +45,27 @@ let negValue = -value
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " ayan"
 
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2"); //link is below down
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+ console.log(+true);
+ console.log(+"");
 
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
+console.log(gameCounter);
 ++gameCounter;
 console.log(gameCounter);
 
