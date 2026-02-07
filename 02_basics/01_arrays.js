230f15a251dcdf9cbf0552b2cf9eddb523c60443
Ayan-Xhmed <ayanahmed9760@gmail.com>
2026-02-07 18:23:44 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// array
+
+const myArr = [1,2,3,4,5,true,"ayan"]
+// console.log(myArr[5]);
+
+// Arry Methods
+
+// myArr.push(6);
+// console.log(myArr);
+
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(8);
+// console.log(myArr);
+
+// myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes("ayans"));
+// console.log(myArr.indexOf(5));
+
+
+const arr1 = myArr.slice(1,3);
+console.log("A",myArr);
+console.log(arr1);
+
+const arr2 = myArr.splice(1,3);
+console.log("B",myArr);
+console.log(arr2);
+
+
+// myArr.shift();
+// console.log(myArr);
+// myArr.shift();
+// console.log(myArr);
+
+
+ 
