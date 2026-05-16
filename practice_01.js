65cc09d64226ca92bd5842ed17ce816f0d2ab8d3
Ayan Ahmed <ayanahmed9761@gmail.com>
2026-05-16 20:34:36 +0530

    course js 1

---
+++ practice_01.js
@@ -0,0 +1,13 @@
+let name = "ayan ahmed";
+console.log(name.length);
+
+console.log(name[0]);
+
+console.log(name[name.length - 1]);
+
+console.log("ayan ahmed"+123);
+
+let space = ""
+console.log(space.length);
+let space1 = "  "
+console.log(space1.length)
